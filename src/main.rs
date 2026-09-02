618ec262dd3b782ee028ee46f3f0a6d4e773650c
Sanjeev Kaushik <sanjeevkaushik90684@gmail.com>
2026-09-02 18:37:17 +0530

    error handling

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::io;
+
+fn main() {
+    
+    let guess = my_num();
+    let num = user_input();
+    match num {
+        Ok(number) => {
+            match_num(number);
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
+}
+
+fn user_input() -> Result<i32, String> {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Enter a vaild number");
+
+    let num = input.trim().parse::<i32>();
+
+    match num {
+        Ok(num) => Ok(num),
+        Err(_) => Err("Please enter a valid number".to_string()),
+    }
+}
+
+fn my_num() -> i32 {
+    let guess: i32 = 25;
+    guess
+}
+
+fn match_num(guess: i32) {
+
+    loop {
+        let x=user_input();
+       match x{
+        Ok(x)=>{
+            if guess==x{
+                println!("You guess right");
+                break;
+            }
+            else {
+                println!("You guess wrong");
+            }
+        }
+        Err(_)=>{
+            println!("invaild number Try Again")
+        }
+       }
+        
+        
+    }
+}
