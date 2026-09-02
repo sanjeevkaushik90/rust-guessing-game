30672c21656d390f1fd984b6422e5e7ba2c98664
Sanjeev Kaushik <sanjeevkaushik90684@gmail.com>
2026-09-02 23:09:34 +0530

    All done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::{random_range, rng};
 
 fn main() {
     let guess = my_num();
@@ -11,36 +12,39 @@ fn user_input() -> Result<i32, String> {
 
     io::stdin()
         .read_line(&mut input)
-        .expect("Enter a vaild number");
+        .map_err(|_|"Failed to read output")?;
 
-    let num = input.trim().parse::<i32>();
+    let num = input.trim().parse::<i32>().map_err(|_|"Enter a vaild number")?;
 
-    match num {
-        Ok(num) => Ok(num),
-        Err(_) => Err("Please enter a valid number".to_string()),
-    }
+   Ok(num)
 }
 
 fn my_num() -> i32 {
-    let guess: i32 = 25;
+    let guess: i32 = rand::random_range(1..10);
     guess
 }
 
 fn match_num(guess: i32) {
+    let mut y=0;
     loop {
         let x = user_input();
         match x {
             Ok(x) => {
                 if guess == x {
                     println!("You guess right");
+                    y+=1;
                     break;
                 } else {
                     println!("You guess wrong");
+                    y+= 1;
                 }
+                
             }
             Err(_) => {
                 println!("invaild number Try Again")
             }
-        }
+        } 
     }
+
+    println!("You guess in {} tries",y)
 }
